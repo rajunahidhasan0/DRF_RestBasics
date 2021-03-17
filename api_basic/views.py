18f044093430f2226c0219ef03727b30bbb0b7ee
rajunahidhasan0 <rajunahidhasan0@gmail.com>
2021-03-17 13:29:59 +0600

    Create venv

--- api_basic/views.py
+++ api_basic/views.py
@@ -6,6 +6,7 @@ from .serializers import ArticleSerializer
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import  status
 # Create your views here.
 
 @api_view(['GET','POST'])
@@ -13,16 +14,15 @@ def article_list(request):
     if request.method == 'GET':
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
-        return JsonResponse(serializer.data, safe =  False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializer(data=data)
+        serializer = ArticleSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @csrf_exempt
